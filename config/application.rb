4b56616ad99e647d99adba4dff2f53965a968522
Fernando <fesilvajacquier@protonmail.com>
2024-07-16 17:39:48 -0300

    add host

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module RailsTnt
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "toucan-feasible-truly.ngrok-free.app"
   end
 end
